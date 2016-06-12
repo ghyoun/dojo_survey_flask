826bd482465cad68fa23b82eb17baf2c28ce214b
ghyoun <ghyoun@uw.edu>
2016-06-11 21:50:06 -0700

    Added survey validations

--- survey.py
+++ survey.py
@@ -12,6 +12,10 @@ def ninja():
     location = request.form['location']
     language = request.form['language']
     comment = request.form['comment']
+    if (len(name) < 1 or len(location) < 1 or len(language) < 1):
+        return 'Error: Input was blank'
+    elif (len(comment) > 120):
+        return 'Error: Comment was too long'
     return render_template('result.html', username=name, user_loc=location, user_lang=language, user_comment=comment)
 
 app.run(debug=True)
